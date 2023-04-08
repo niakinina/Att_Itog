5ad74ac96a2d685c7cf09547cfd76a7207931a64
niakinina <2035-n.i.akinina@mpt.ru>
2023-04-09 01:27:21 +0300

    Создан проект

---
+++ Att_Itog/src/main/java/com/example/att_itog/AttItogApplication.java
@@ -0,0 +1,13 @@
+package com.example.att_itog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttItogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttItogApplication.class, args);
+    }
+
+}
