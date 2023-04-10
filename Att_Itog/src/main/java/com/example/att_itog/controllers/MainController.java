d244d576b35d37f8e900c8f3f144ec51abba3e66
niakinina <2035-n.i.akinina@mpt.ru>
2023-04-11 02:17:46 +0300

    Добавлена страничка для проверки логина и пароля, созданы классы Main Controller и Person, создана и привязана база данных, в которой теперь есть 3 пользователя

---
+++ Att_Itog/src/main/java/com/example/att_itog/controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.example.att_itog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
---
+++ Att_Itog/src/main/java/com/example/att_itog/models/Person.java
@@ -0,0 +1,47 @@
+package com.example.att_itog.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="Person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Логин не может быть пустым")
+    @Size(min = 5, max = 100, message = "Логин должен быть от 5 до 100 символов")
+    @Column(name = "login")
+    private String login;
+
+    @NotEmpty(message = "Пароль не может быть пустым")
+    @Column(name="password")
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
