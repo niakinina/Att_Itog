6919ab2aaaa4925cedb2ac5dff0c50ca30ba6ca5
niakinina <2035-n.i.akinina@mpt.ru>
2023-04-11 20:28:26 +0300

    В данной версии мы применяем одну форму аутентификации, есть комментарий для кастомной аутентификации, которая возможно потом пригодится (нужно будет посмотреть), со следующей версии будет применяться другая (своя) форма аутентификации. Создана страничка authentication.html Первое изменение по форме будет в секьюрити конфиг.

--- Att_Itog/src/main/java/com/example/att_itog/config/SecurityConfig.java
+++ Att_Itog/src/main/java/com/example/att_itog/config/SecurityConfig.java
@@ -1,20 +1,27 @@
 package com.example.att_itog.config;
 
-import org.springframework.security.authentication.AuthenticationProvider;
+import com.example.att_itog.services.PersonDetailService;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration{
+    private final PersonDetailService personDetailService;
 
-    private final AuthenticationProvider authenticationProvider;
-
-    public SecurityConfig(AuthenticationProvider authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
+    public SecurityConfig(PersonDetailService personDetailService) {
+        this.personDetailService = personDetailService;
     }
 
-    protected void configuration(AuthenticationManagerBuilder authenticationManagerBuilder){
-        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
+//    private final AuthenticationProvider authenticationProvider;
+
+//    public SecurityConfig(AuthenticationProvider authenticationProvider) {
+//        this.authenticationProvider = authenticationProvider;
+//    }
+
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
+        authenticationManagerBuilder.userDetailsService(personDetailService);
+
     }
 }
--- Att_Itog/src/main/java/com/example/att_itog/controllers/MainController.java
+++ Att_Itog/src/main/java/com/example/att_itog/controllers/MainController.java
@@ -1,12 +1,29 @@
 package com.example.att_itog.controllers;
 
+import com.example.att_itog.config.SecurityConfig;
+import com.example.att_itog.security.PersonDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class MainController {
     @GetMapping("/index")
-    public String index(){
+    public String index() {
+        // Получаем объект аутентификации
+        // с помощью спрингконтекстхолдер обращаемся к контексту и на нем вызываем метод аутентификации
+        // Из сессии текуего пользователя получаем объект, кот. был положен в данную сесию после аутентиф-ии пользователя
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+        System.out.println("ID пользователя: " + personDetails.getPerson().getId());
+        System.out.println("Логин пользователя: " + personDetails.getPerson().getLogin());
+        System.out.println("Пароль пользователя: " + personDetails.getPerson().getPassword());
+
         return "index";
+
     }
 }
