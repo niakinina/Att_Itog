f53b9c0d78d159283d5fa53503451780ec9058cf
niakinina <2035-n.i.akinina@mpt.ru>
2023-04-13 01:58:12 +0300

    Была добавлена страничка добавления продукта, сделаны поля для заполнения и добавления фотографий. Фотографии ещё не добавляла, в следующей версии укажу путь к добавлению фоток

--- Att_Itog/src/main/java/com/example/att_itog/controllers/AdminController.java
+++ Att_Itog/src/main/java/com/example/att_itog/controllers/AdminController.java
@@ -1,20 +1,29 @@
 package com.example.att_itog.controllers;
 
 import com.example.att_itog.models.Product;
+import com.example.att_itog.repositories.CategoryRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+    private final CategoryRepository categoryRepository;
+
+    public AdminController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("admin/product/add")
+    public String addProduct(Model model) {
+        model.addAttribute("product", new Product());
+        model.addAttribute("category", categoryRepository.findAll());
+        return "product/addProduct";
+    }
+
     @GetMapping("/admin")
     public String admin(){
         return "admin";
     }
 
-    @GetMapping("admin/product/add")
-    public String addProduct(Model model){
-        model.addAttribute("product", new Product());
-        model.addAttribute("category");
-    }
 }
--- Att_Itog/src/main/java/com/example/att_itog/controllers/AuthenticationController.java
+++ Att_Itog/src/main/java/com/example/att_itog/controllers/AuthenticationController.java
@@ -1,21 +1,23 @@
 package com.example.att_itog.controllers;
 
-import com.example.att_itog.models.Person;
+import com.example.att_itog.models.Product;
 import com.example.att_itog.repositories.CategoryRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class AuthenticationController {
 
-    private final CategoryRepository
+
 
     @GetMapping("/authentication")
     public String login(){
         return "authentication";
     }
+
+
+
 
 
 }
